061dc450e1e9372897b8d04dc15a26e3e744aa93
junh0328 <junh0328@naver.com>
2020-09-04 20:28:29 +0900

    chapter 11.2 - index.pug를 통해 클라이언트와 서버의 연결 - chrome으로 확인할 것

--- chapter04/socket.js
+++ chapter04/socket.js
@@ -1,22 +1,23 @@
 const WebSocket = require('ws');
 
 module.exports = (server) => {
-    const wss = new WebSocket.Server({ server });
+    const wss = new WebSocket.Server({ server });   //웹 소켓 서버
 
     wss.on('connection', (ws, req) => {
+        //req.headers['x-forwarded-for'] || req.connection.remoteAddress는 클라이언트의 IP를 알아내는 방법 중 하나이다. 
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         console.log('새로운 클라이언트 접속', ip);
-        ws.on('message', (message) => {
+        ws.on('message', (message) => {     // 클라이언트로 부터 메세지가 왔을 때 발생하는 이벤트 리스너
             console.log(message);
         });
-        ws.on('error', (error) => {
+        ws.on('error', (error) => {         // 웹 소켓 연결 중 문제가 생겼을 때 발생하는 이벤트 리스너
             console.error(error);
         });
-        ws.on('close', () => {
+        ws.on('close', () => {              // 클라이언트와 연결이 끊겼을 때 발생하는 이벤트 리스너, interval 메소드를 clearInterval로 종료시킴
             console.log('클라이언트 접속 해제', ip);
             clearInterval(ws.interval);
         });
-        const interval = setInterval(() => {
+        const interval = setInterval(() => {    // 3초마다 연결된 모든 클아이언트에게 메시지를 보내는 메소드 interval
             if (ws.readyState === ws.OPEN) {
                 ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
             }
